52bbacdc07d6ad3bac3f8c559c666fdbbd9ac0e8
Stuart Knightley <stuart@stuartk.com>
2013-02-15 15:05:47 -0800

    Add comment explaining sub build scripts

--- build.sh
+++ build.sh
@@ -14,8 +14,21 @@ master_hash=`git rev-parse --short HEAD`
 echo "Update to master $master_hash" >> "$TEMP_DIR/COMMIT_MESSAGE"
 echo "" >> "$TEMP_DIR/COMMIT_MESSAGE"
 
+#####################################################################
+# Add sub-build scripts here
+# These environment variables are available:
+#
+# $TEMP_DIR
+#           Directory to place temporary build files
+# $TEMP_DIR/COMMIT_MESSAGE
+#           Append to this file to add to the commit message
+# $OUT_DIR
+#           Directory to put new output files. At the end of the build all
+#           files in this directory are copied into the gh-pages branch
+
 ./docs/build/build.sh
-cd "$ROOT_DIR"
+
+#####################################################################
 
 if [ "$1" == "--no-commit" ]
 then
@@ -43,7 +56,7 @@ git commit --file="$TEMP_DIR/COMMIT_MESSAGE" || :
 
 git checkout -
 
-rm -r $OUT_DIR
-rm -r $TEMP_DIR
+rm -rf $OUT_DIR
+rm -rf $TEMP_DIR
 
 echo "Done."
