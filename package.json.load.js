f1c375a74a211e52b404eb95a02f8dcc7f0c4714
simurai <simulus@gmail.com>
2013-02-25 21:51:22 +0100

    Update to master 3bd7835
    wiki: e505a7c

--- package.json.load.js
+++ package.json.load.js
@@ -1,1 +1,1 @@
-montageDefine("574d69b","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"574d69b",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"1d1016a",location:"packages/montage@1d1016a/"}},production:!0,useScriptInjection:!0}})
+montageDefine("e3acbc9","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"e3acbc9",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"1d1016a",location:"packages/montage@1d1016a/"}},production:!0,useScriptInjection:!0}})
--- ui/themes.reel/themes.load.js
+++ ui/themes.reel/themes.load.js
@@ -1,1 +1,1 @@
-montageDefine("574d69b","ui/themes.reel/themes",{dependencies:["montage","montage/ui/component"],factory:function(e,t,n){var r=e("montage").Montage,i=e("montage/ui/component").Component;t.Themes=r.create(i,{hasTemplate:{value:!0}})}})
+montageDefine("e3acbc9","ui/themes.reel/themes",{dependencies:["montage","montage/ui/component"],factory:function(e,t,n){var r=e("montage").Montage,i=e("montage/ui/component").Component;t.Themes=r.create(i,{hasTemplate:{value:!0}})}})
