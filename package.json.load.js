5b5c826cc9040a63034f19a445282cb186434787
Stuart Knightley <stuart@stuartk.com>
2013-03-11 15:57:03 -0700

    Update to master 9f994db
    wiki: 6960fd6

--- package.json.load.js
+++ package.json.load.js
@@ -1,1 +1,1 @@
-montageDefine("5a99534","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"5a99534",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"0d362fa",location:"packages/montage@0d362fa/"}},production:!0,useScriptInjection:!0}})
+montageDefine("e85829a","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"e85829a",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"0d362fa",location:"packages/montage@0d362fa/"}},production:!0,useScriptInjection:!0}})
