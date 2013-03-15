677a45a33c3ff1c6efa500ececb1f45a1e1480a0
Stuart Knightley <stuart@stuartk.com>
2013-03-15 11:50:31 -0700

    Update to master 4cbb08b
    wiki: a394a39

--- package.json.load.js
+++ package.json.load.js
@@ -1,1 +1,1 @@
-montageDefine("5e61a4c","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"5e61a4c",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"8c9da00",location:"packages/montage@8c9da00/"}},production:!0,useScriptInjection:!0}})
+montageDefine("7467bd6","package.json",{exports:{name:"montagejs.org",version:"0.0.0",hash:"7467bd6",directories:{lib:"./"},mappings:{montage:{name:"montage",hash:"8c9da00",location:"packages/montage@8c9da00/"}},production:!0,useScriptInjection:!0}})
--- ui/themes.reel/themes.load.js
+++ ui/themes.reel/themes.load.js
@@ -1,1 +1,1 @@
-montageDefine("5e61a4c","ui/themes.reel/themes",{dependencies:["montage","montage/ui/component"],factory:function(e,t,n){var r=e("montage").Montage,i=e("montage/ui/component").Component;t.Themes=r.create(i,{hasTemplate:{value:!0}})}})
+montageDefine("7467bd6","ui/themes.reel/themes",{dependencies:["montage","montage/ui/component"],factory:function(e,t,n){var r=e("montage").Montage,i=e("montage/ui/component").Component;t.Themes=r.create(i,{hasTemplate:{value:!0}})}})
