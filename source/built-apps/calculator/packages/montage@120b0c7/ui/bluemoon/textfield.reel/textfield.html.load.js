cd186c26e7d43227312558e6e203d93580f0144f
António Afonso <antonio.afonso@gmail.com>
2013-03-31 12:24:41 -0700

    Add built application calculator
    This application has Q with stacktrace support disabled.
    
    Origin: https://github.com/montagejs/calculator/commit/52cba8c2daa116431690eabd26bfd9e042aaaa13

---
+++ source/built-apps/calculator/packages/montage@120b0c7/ui/bluemoon/textfield.reel/textfield.html.load.js
@@ -0,0 +1,1 @@
+montageDefine("120b0c7","ui/bluemoon/textfield.reel/textfield.html",{text:"<!DOCTYPE html>\n\n<html>\n<head>\n    <meta charset=utf-8>\n    <link rel=stylesheet href=textfield.css>\n</head>\n<body>\n</body>\n</html>"})
---
+++ source/built-apps/calculator/packages/montage@120b0c7/ui/input-radio.reel/input-radio.html.load.js
@@ -0,0 +1,1 @@
+montageDefine("120b0c7","ui/input-radio.reel/input-radio.html",{text:"<!DOCTYPE html>\n\n<html>\n<head>\n    <meta charset=utf-8>\n    <link rel=stylesheet href=input-radio.css>\n</head>\n<body>\n</body>\n</html>"})
